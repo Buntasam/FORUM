06f50bad81cb889205f2da4f81e155c26d8e02b4
Buntasam <144217405+Buntasam@users.noreply.github.com>
2024-06-20 09:11:41 +0200

    test

--- backend.js
+++ backend.js
@@ -1,46 +1,55 @@
-const express = require('express')
-const app = express()
-const DBmanager = require('./public/js/dbmanager.js')
+const express = require('express');
+const app = express();
+const DBmanager = require('./public/js/dbmanager.js');
+const bodyParser = require('body-parser'); // Ajout de body-parser
 
 // socket.io setup
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io')
-const mysql = require("mysql");
-const io = new Server(server, {pingInterval: 2000, pingTimeout: 5000})
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 });
 
-const port = 3000
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json()); // Utilisation de body-parser pour parser les données JSON
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/html/createaccount.html')
-})
+  res.sendFile(__dirname + '/public/html/createaccount.html');
+});
 
-const db = new DBmanager("localhost","root","")
+const db = new DBmanager("localhost", "root", "");
 
-db.insertUser("Meca","motdepassesolide","membre")
+// Route pour gérer la création d'utilisateur
+app.post('/createUser', (req, res) => {
+  const { username, password, role } = req.body;
+  db.insertUser(username, password, role)
+    .then(() => {
+      res.status(200).send({ message: 'User created successfully' });
+    })
+    .catch(err => {
+      res.status(500).send({ message: 'Error creating user', error: err });
+    });
+});
 
-const backendPlayers = {}
+const backendPlayers = {};
 
 io.on('connection', (socket) => {
-  socket.on('initgame', ({username}) => {
+  socket.on('initgame', ({ username }) => {
     backendPlayers[socket.id] = {
       username
-    }
+    };
     console.log(backendPlayers);
-  })
-  io.emit('updatePlayers', backendPlayers)
-
+  });
+  io.emit('updatePlayers', backendPlayers);
 
   socket.on('disconnect', reason => {
     console.log(reason);
-    delete backendPlayers[socket.id]
-    io.emit('updatePlayers', backendPlayers)
-
-  })
+    delete backendPlayers[socket.id];
+    io.emit('updatePlayers', backendPlayers);
+  });
 });
 
 server.listen(port, () => {
-  console.log('http://localhost:3000')
-})
+  console.log('http://localhost:3000');
+});
